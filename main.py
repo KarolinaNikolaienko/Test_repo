11d32d28ee751957cb58439ed367604a0a2858cc
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2023-11-05 23:29:17 +0100

    ver3_Cesar

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello world")
-
-print("Hello Git")
+message = input("Enter a message: ")
+offset = int(input("Enter the offset: "))
+encoded_message = ""
+for ch in message:
+    if ord(ch) >= ord('A') and ord(ch) <= ord('Z'):
+        pos = ord(ch) - ord('A')
+        pos = (pos + offset) % 26
+        encoded_message += chr(pos + ord('A'))
+    elif ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+        pos = ord(ch) - ord('a')
+        pos = (pos + offset) % 26
+        encoded_message += chr(pos + ord('a'))
+    else:
+        encoded_message += ch
+print(encoded_message)
